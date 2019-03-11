ba3f2334ab763dedb33a3cf84b7343af261c26ec
Alessandro <Alessandro@Asus_Ale>
2019-03-11 22:21:04 +0100

    Add voto fine lezione

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,25 @@
 package it.polito.tdp.libretto;
 
-public class Libretto {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto {
+	
+	private List<Voto> voti ;
+	
+	public Libretto() {
+		
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	public void add(int voto , String corso, LocalDate data ) {
+		
+	}
+	
+	
+	public void add(Voto v) {
+		
+		
+	}
 }
